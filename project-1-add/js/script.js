c0741c47ec50c7109a38e86f2c36644a9f45736b
Nadia <nani.feribird@gmail.com>
2018-11-17 15:29:31 +0200

    add 1 lesson

--- project-1-add/js/script.js
+++ project-1-add/js/script.js
@@ -1,4 +1,4 @@
-var num = 33721;
+let num = 33721;
 // console.log(num);
 
 // создает массив из строки:
@@ -7,43 +7,43 @@ var num = 33721;
 
 // почему тогда этот работает?? 
 // если split преобразовывает тоже строки
-var arr = String(num).split(""); 
+let arr = String(num).split(""); 
 // console.log(arr);
 
-var prod = 1;
-for(var i = 0; i < arr.length; i++){
+let prod = 1;
+for(let i = 0; i < arr.length; i++){
     prod *= arr[i];
     }
 console.log(prod);
 
-var value = String(prod ** 3).split("");
+let value = String(prod ** 3).split("");
 // console.log(value);
 
 console.log(value[0] + value[1]);
 
-// глобальная переменная
-var q = 1;
-console.log(q);
-{
-   var q = 3;
-   console.log(q);
-}
-console.log(q);
+// // глобальная переменная
+// var q = 1;
+// console.log(q);
+// {
+//    var q = 3;
+//    console.log(q);
+// }
+// console.log(q);
 
-// локальная переменная??
-let m = 1;
-console.log(m);
-{
-   let m = 3;
-   console.log(m);
-}
-console.log(m);
+// // локальная переменная??
+// let m = 1;
+// console.log(m);
+// {
+//    let m = 3;
+//    console.log(m);
+// }
+// console.log(m);
 
-// чем отличается let и const?
-const p = 1;
-console.log(p);
-{
-   const p = 3;
-   console.log(p);
-}
-console.log(p);
+// // чем отличается let и const?
+// const p = 1;
+// console.log(p);
+// {
+//    const p = 3;
+//    console.log(p);
+// }
+// console.log(p);
--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var expensesUnit1 = prompt("Введите обязательную статью расходов в этом месяце?", "скрепка");
-var expensesCost1 = +prompt("Во сколько обойдется?", "1.25");
-var expensesUnit2 = prompt("Введите обязательную статью расходов в этом месяце?", "кнопка");
-var expensesCost2 = +prompt("Во сколько обойдется?", "1.75");
+let expensesUnit1 = prompt("Введите обязательную статью расходов в этом месяце?", "скрепка");
+let expensesCost1 = +prompt("Во сколько обойдется?", "1.25");
+let expensesUnit2 = prompt("Введите обязательную статью расходов в этом месяце?", "кнопка");
+let expensesCost2 = +prompt("Во сколько обойдется?", "1.75");
 
-var appData = {
+let appData = {
    money: +prompt("Какой ваш бюджет на месяц?", "500"),
    time: prompt("Введите дату в формате YYYY-MM-DD", "2018-10-08"),
    expenses: {
@@ -17,6 +17,5 @@ var appData = {
    savings: false
 };
 
-var value = (appData.moneyData - 
-   (appData.expenses.expensesUnit1 + appData.expenses.expensesUnit2)) / 30;
+let value = (appData.money / 30);
 console.log(value);
