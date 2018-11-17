b64a5dba0ee784ab3104494b51b143bd1d31bea5
Nadia <nani.feribird@gmail.com>
2018-11-17 19:55:35 +0200

    add 1 lesson

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,21 +1,18 @@
 "use strict";
 
-let expensesUnit1 = prompt("Введите обязательную статью расходов в этом месяце?", "скрепка");
-let expensesCost1 = +prompt("Во сколько обойдется?", "1.25");
-let expensesUnit2 = prompt("Введите обязательную статью расходов в этом месяце?", "кнопка");
-let expensesCost2 = +prompt("Во сколько обойдется?", "1.75");
-
 let appData = {
    money: +prompt("Какой ваш бюджет на месяц?", "500"),
    time: prompt("Введите дату в формате YYYY-MM-DD", "2018-10-08"),
-   expenses: {
-    expensesUnit1: expensesCost1,
-    expensesUnit2: expensesCost2
-   },
+   expenses: {},
    optionalExpenses: {},
    income: [],
    savings: false
 };
+
+// let unit1 = prompt("Введите обязательную статью расходов в этом месяце?", "скрепка");
+// let unit2 = prompt("Введите обязательную статью расходов в этом месяце?", "кнопка");
+appData.expenses[prompt("Введите обязательную статью расходов в этом месяце?", "скрепка")] = +prompt("Во сколько обойдется?", "1.25");
+appData.expenses[prompt("Введите обязательную статью расходов в этом месяце?", "кнопка")] = +prompt("Во сколько обойдется?", "1.75");
 
 let value = (appData.money / 30);
 console.log(value);
